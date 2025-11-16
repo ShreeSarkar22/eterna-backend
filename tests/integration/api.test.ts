c989a7bb242b99b58e23e20690fa51aef6e4a59b
ShreeSarkar22 <shreesarkar2020@gmail.com>
2025-11-16 19:46:36 +0530

    Fixed errors

--- tests/integration/api.test.ts
+++ tests/integration/api.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import express from 'express';
 import { tokenController } from '../../src/controllers/tokenController';
 import { errorHandler } from '../../src/middleware/errorHandler';
+import { describe, it } from 'node:test';
 
 // Mock services
 jest.mock('../../src/services/aggregationService');
